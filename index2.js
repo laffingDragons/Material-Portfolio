fb8234356a66d170a0a52879d83f52093fb01681
laffingDragons <akshaypatil.vit@gmail.com>
2022-06-30 23:17:27 +0530

    Constructor function

--- index2.js
+++ index2.js
@@ -39,16 +39,66 @@
 
 
 // Settimeout let block scope explained 
-function settimeooutBlockScope() {
+// function settimeooutBlockScope() {
     
-    // for (var i = 0; i <= 5; i++) {
-        // let has blocked scope 
-        for (let i = 0; i <= 5; i++) {
-       setTimeout(() => {
-        console.log(i);
-       }, i * 1000);
-    }
-};
+//     // for (var i = 0; i <= 5; i++) {
+//         // let has blocked scope 
+//         for (let i = 0; i <= 5; i++) {
+//        setTimeout(() => {
+//         console.log(i);
+//        }, i * 1000);
+//     }
+// };
 
-settimeooutBlockScope();
+// settimeooutBlockScope();
 // Settimeout let block scope explained  Ends here
+
+
+// Data hiding or privacy function starts here 
+
+// function counter() {
+//     var count = 0;
+//     return  function increamentCounter() {
+//         count++;
+//         console.log(count);
+//     }
+// }
+
+// let counter1 = counter();
+// counter1();
+// counter1();
+// counter1();
+
+// let counter2 = counter();
+// counter2();
+// counter2();
+
+// Data hiding or privacy function Ends here 
+
+
+// Function Constructor in javascript  starts here
+
+function Counter() {
+
+    var count = 10;
+
+    this.increamentCounter = function() {
+        count++;
+        console.log(count);
+    }
+
+    this.decreamentCounter = function () {
+        count--;
+        console.log(count);
+    }
+}
+
+let counter1 = new Counter();
+
+counter1.increamentCounter();
+counter1.increamentCounter();
+counter1.increamentCounter();
+
+counter1.decreamentCounter();
+
+// Function Constructor in javascript  ends here
